3c13203ec1a8f2e4b9db8287e66af924c6591b82
LuisFaz <alejandro.faz@hotmail.com>
2024-01-21 20:51:45 -0500

    Added Random Numbers

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+var targetValue = 0
+
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
     }
     @IBAction func showAlert() {
-      let alert = UIAlertController(
-        title: "Hello, World",
-        message: "This is my first app!",
-        preferredStyle: .alert)
-      let action = UIAlertAction(
-        title: "Awesome",
-        style: .default,
-        handler: nil)
-      alert.addAction(action)
-      present(alert, animated: true, completion: nil)
+      let message = "The value of the slider is: \(currentValue)" +
+                    "\nThe target value is: \(targetValue)"
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: message,    // changed
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "OK",          // changed
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        startNewRound()
     }
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    func startNewRound() {
+      targetValue = Int.random(in: 1...100)
+      currentValue = 50
+      slider.value = Float(currentValue)
+    }
+    @IBOutlet var slider: UISlider!
 }
+
+
 
